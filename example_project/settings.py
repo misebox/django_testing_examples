975da2b0764725dfa1e4aa4517bcf7748f63186c
misebox <misebox@gmail.com>
2021-08-27 22:27:25 +0900

    Add logging setting but off it

--- example_project/settings.py
+++ example_project/settings.py
@@ -131,3 +131,22 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': [
+                #'console'
+            ],
+            'level': 'DEBUG',
+        },
+    },
+}
