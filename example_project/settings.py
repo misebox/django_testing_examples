2d1efdc5b4a63f2fed5bbd945bde83ff53dc300d
misebox <misebox@gmail.com>
2021-08-29 12:37:47 +0900

    Now django gets secret key from Environment variable

--- example_project/settings.py
+++ example_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from typing import List
 from pathlib import Path
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&0&!*8m3h%w$d-!@82nu6k*7t+b=rh75+knw1zvdhs5)ofjqu+'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
